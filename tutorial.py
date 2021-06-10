107a4cf78397d377f032c67a1c786acaca7da916
duktar210143 <duktar210143@gmail.com>
2021-06-10 21:25:13 +0545

    Python tutorial file added

---
+++ tutorial.py
@@ -0,0 +1,77 @@
+# name=input("what is your name \n?")
+# fav_colour=input("what is your favourite colour\n")
+# print("hi" + name + "so your fav colour is " + fav_colour)
+
+# birth_year=int( input("birth year:"))
+# age= 2022-birth_year
+# print(type(birth_year))
+# print(type(age))
+# print(f"your age is {age}")
+
+## ask a user tgeir weight (in pounds), convert it to kilogarms
+
+# weight=int(input("enter your wwight in pounds:\n"))
+# you_weight=weight*0.45
+# print(f"your weight in kilo is {you_weight}")
+
+
+# simple methods used in pyhon
+# len()
+# course.upper()
+# course.lower()
+# course.title()
+# course.find()
+# course.replace
+# "..." in course
+
+# #ogmented operatoprs
+# x=int(input("your number:\n"))
+# x+=3
+# print(x)
+# import math
+# print(math.ceil(2.9))
+# x=2.9
+# print(round(x))
+# print(abs(-2.9))
+
+#if elif else
+# is_hot= False #(True)
+# is_cold= True #(True)
+#
+#
+# if is_hot:
+#     print("it's a hot day")
+#     print("drink a lot of water")
+# elif is_cold:
+#     print("it is cold day")
+#     print("wear your sweater")
+# else:
+#     print("it's neither cold nor hot ")
+# print("it's a lovely day")
+#
+
+
+#price of a house is 1m if buyer has good credit,thry
+#need to put down 10% otherwise 20%
+
+# price=100000
+# has_good_credit=False
+#
+# if has_good_credit:
+#     down_payment=0.1*price
+# else:
+#     down_payment=0.2*price
+# print(f"down payment ${down_payment}")
+
+
+
+
+
+
+
+
+
+
+
+
+
